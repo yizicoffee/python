ea2dbbaf388b9d975d23a660b4495da9ca7b3f58
yucheng <>
2020-01-07 14:59:57 +0800

    study calculate

--- pow_try.py
+++ pow_try.py
@@ -5,3 +5,13 @@ print(2 - 2)
 print(2 + 2)
 print(3 // 2)
 print(2 ** 4)
+# 快速赋值 x op=y . x += y  means x = x + y
+x = 3
+x /= 3
+print(x)
+# 绝对值
+print(abs(-100))
+# 商余操作
+print(divmod(10, 3))
+# 四舍五入
+print(round(-10.231,2))
